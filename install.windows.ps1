ac166a9fbceff77d33015df2f3eea93ebfb245ca
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-08-15 17:19:27 +0000

    e most of install windows

--- install.windows.ps1
+++ install.windows.ps1
@@ -1,32 +1,17 @@
 # To run this script directly, run this in an elevated admin PowerShell prompt:
-#     Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/approvals/ApprovalTests.Python/master/install.windows.ps1 | Invoke-Expression
+#     Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/LearnWithLlew/MobTime.keyboardswitching/main/install.windows.ps1 | Invoke-Expression
 
 # This script is intended to setup a dev machine from scratch. Very useful for setting up a EC2 instance for mobbing.
 #
 
-
-iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
-# iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/python-pycharm.ps1 | iex
-
 iwr -useb cin.st | iex
 choco feature enable --name=allowGlobalConfirmation
-choco install beyondcompare
-choco install python --version=3.6.7 
-choco install pip
-choco install pycharm
 
-syspin "C:\Program Files (x86)\JetBrains\PyCharm 2020.3.2\bin\pycharm64.exe" "Pin to taskbar"
-
-choco install visualstudio2019buildtools
-pip install tox
+choco install anyhotkey
 
 # Clone repo
-& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/approvals/ApprovalTests.Python.git C:\Code\ApprovalTests.Python
-cd C:\Code\ApprovalTests.Python
-
-tox -e dev
-tox -e lint
-
+& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/LearnWithLlew/MobTime.keyboardswitching.git C:\Code\MobTime.keyboardswitching
+export MOBTIME_SCRIPT=c:\code\MobTime.keyboardswitching\rotate_{name}.bat
 # Done
 cls
 echo "Done!"
